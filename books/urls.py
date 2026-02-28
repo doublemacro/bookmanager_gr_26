50d2cb3795d1e105e11722a928aee33916ac6723
antochi <adrian.constantin.antochi@gmail.com>
2026-02-28 11:19:48 +0200

    updated css

--- books/urls.py
+++ books/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('book/<int:pk>/update/', views.update_book, name="update_book"),
     path('api_simple_endpoint/', views.simple_endpoint, name="simple"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
